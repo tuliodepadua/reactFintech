ce25dead222777a3c568ded362c15f1ebc500aa1
tulio.de.padua.silva <tulio.de.padua.silva@accenture.com>
2019-06-25 10:02:28 -0300

    ajuste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Estrutura from './organisms/home';
 import './App.scss';
-import 'materialize-css/dist/css/materialize.min.css';
+import './materialize-css/dist/css/materialize.min.css';
 import Aplicacao from './styles'
 
 function App() {
