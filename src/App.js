29af65b3dc21355047a70349429f89a9dbd261e4
tulio.de.padua.silva <tulio.de.padua.silva@accenture.com>
2019-06-25 10:20:48 -0300

    Ajuste url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Estrutura from './organisms/home';
 import './App.scss';
-import './materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import Aplicacao from './styles'
 
 
 function App() {
-  
+
   return (
     <Aplicacao className="App">
       <Estrutura />
